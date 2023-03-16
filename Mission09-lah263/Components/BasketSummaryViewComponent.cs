483aab43d5be452555131d2931fec2a10ed43fa5
lillie-ann <lah263@byu.edu>
2023-03-15 23:02:28 -0600

    extra

---
+++ Mission09-lah263/Components/BasketSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_lah263.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_lah263.Components
+{
+    public class BasketSummaryViewComponent : ViewComponent {
+        private Basket basket;
+    public BasketSummaryViewComponent(Basket b)
+    {
+        basket = b;
+    }
+    public IViewComponentResult Invoke()
+    {
+        return View(basket);
+    }
+
+    }
+}
